6d2ad6d2b67ca3128cbb4be1e55a39ebfd70ac8d
UmairAnwar317 <umairanwar317@gmail.com>
2020-06-15 23:31:22 +0500

    js assinment Display

---
+++ app.js
@@ -0,0 +1,62 @@
+// === Chapter # 1 Task # 1
+
+// alert("Wellcome to java script Assigment");
+
+// Chapter # 1 Task # 2
+
+// alert("Error! Please enter a valid passward");
+
+// Chapter # 1 Task # 3
+
+// alert("Wellcome to Js Land...\n Happy Coding?");
+
+// Chapter # 1 Task # 4
+
+//alert("Wellcome to Js Land... ");
+//alert(" Happy Coding \n Prevent this page from  creating additional dialogs");
+
+// Chapter # 1 Task # 5
+
+//console.log("Hello... I can run Js through my Web browser Console");
+
+
+// Chapter # 2 Task # 3
+
+//var message = " Hello World ";
+//alert(message)
+
+// Chapter # 2 Task # 4
+//var  SName = " Jhone deo";
+//var  SAge = "15 Years Old ";
+//var  certification  = " Certified Mobile Application Development ";
+//alert( SName)
+//alert (SAge)
+//alert(certification)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
